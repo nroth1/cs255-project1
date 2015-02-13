fca77bd8bbb15dbd687e0fedfe9ab2ed6a64ccb5
Michel Hong <yanshu.hong.michael@gmail.com>
2015-02-12 23:54:39 -0800

    finished everything

--- password-manager.js
+++ password-manager.js
@@ -1,7 +1,6 @@
 "use strict";
 
 
-
 /********* External Imports ********/
 
 var lib = require("./lib");
@@ -38,17 +37,17 @@ var KDF = lib.KDF,
 var keychain = function() {
   // Class-private instance variables.
   var priv = {
-    secrets: { SHA_hash:'',salt:'', 
-            key_AUTH_message:'', 
-            key_HMAC_message:'', key_GCM_message:'' },
-    data: {  }
+    secrets: { salt:"", key_AUTH_message:"", key_HMAC_message:"", key_GCM_message:""},
+    data:    { }
   };
 
   // Maximum length of each record in bytes
   var MAX_PW_LEN_BYTES = 64;
-  var HMAC_LENGTH = -1;
-  var MINIMUM_PAD_LEGNTH = 1; 
-  
+  var EXTRA_PADDING_BYTES = 4;
+  var TOTAL_PASSWORD_LENGTH = MAX_PW_LEN_BYTES + 1 + EXTRA_PADDING_BYTES;
+  var AES_KEY_LENGTH_BITS = 128;
+  var MAC_KEY_LENGTH_BITS = 256;
+
   // Flag to indicate whether password manager is "ready" or not
   var ready = false;
 
@@ -63,18 +62,20 @@ var keychain = function() {
     * Return Type: void
     */
   keychain.init = function(password) {  /*N*/
+    ready = false;
+
+    priv.secrets = {};
+    priv.data = {};
+    priv.secrets.version = "CS 255 Password Manager v2.0";
+
+    priv.secrets.salt = lib.random_bitarray(AES_KEY_LENGTH_BITS); 
+    var master_key = lib.KDF(password, priv.secrets.salt);
+
+    priv.secrets.key_AUTH_message = bitarray_slice(lib.HMAC(master_key,"AUTH TO KEY"), 0, AES_KEY_LENGTH_BITS);
+    priv.secrets.key_HMAC_message = bitarray_slice(lib.HMAC(master_key,"HMAC TO KEY"), 0, MAC_KEY_LENGTH_BITS);
+    priv.secrets.key_GCM_message  = bitarray_slice(lib.HMAC(master_key,"GCM TO KEY"),  0, AES_KEY_LENGTH_BITS);
+
     ready = true;
-    priv.secrets = {}
-    priv.data = {}
-    priv.secrets.salt = lib.random_bitarray(128); 
-    var master_key = lib.KDF(password,priv.secrets.salt);
-    //priv.secrets.auth_message = "Authenticated correctly";
-    priv.secrets.key_AUTH_message = bitarray_slice(lib.HMAC(master_key,"AUTH TO KEY"),0,128);
-    priv.secrets.key_HMAC_message = bitarray_slice(lib.HMAC(master_key,"HMAC TO KEY"),0,128);
-    priv.secrets.key_GCM_message = bitarray_slice(lib.HMAC(master_key,"GCM TO KEY"),0,128);
-    
-    //var cipher = lib.setup_cipher(bitarray_slice(master_key,0,128));
-    priv.secrets.version = "CS 255 Password Manager v1.0";
   };
 
   /**
@@ -95,48 +96,52 @@ var keychain = function() {
     * Return Type: boolean
     */
   keychain.load = function(password, repr, trusted_data_check) { /*H*/
+    ready = false;
+
     /* generate the main KEY from the given password. */ 
-    var data = JSON.parse(repr)
-    var salt = data['salt']
+    var data = JSON.parse(repr);
+
+    var salt = data["salt"];
     var master_key = lib.KDF(password,salt);
-    var key_AUTH_message = bitarray_slice(lib.HMAC(master_key,"AUTH TO KEY"),0,128);
-    
+    var key_AUTH_message = bitarray_slice(lib.HMAC(master_key,"AUTH TO KEY"), 0, AES_KEY_LENGTH_BITS);
+
     /* check the correctness of the main KEY. */
-    var cipher = setup_cipher(key_AUTH_message);	
-    var authenticated_output = dec_gcm(cipher, data['auth_message']) 
-     
-
-    if (!bitarray_equal(authenticated_output ,string_to_bitarray('AUTHENTICATE'))) {
-	//should set ready = false too?!
-	ready = false 
-    	return false;
-    }
-
+    var cipher = setup_cipher(key_AUTH_message);
+    try {
+      var authenticated_output = dec_gcm(cipher, data["auth_message"]);  
+    } catch(err) {
+      return false;
+    }   
+    if (!bitarray_equal(authenticated_output, string_to_bitarray("AUTHENTICATE"))) return false;
+    
     /* check the integrity of KVS data. */
     if (trusted_data_check) {
         /* noraml way: compute the checksum of the repr string and compare it with the 
         provided SHA-256 hash. */
         var hash_value = SHA256(string_to_bitarray(repr))
-        if (! bitarray_equal(hash_value,trusted_data_check)) throw "INVALID REPR!";
-        
+        if (!bitarray_equal(hash_value, trusted_data_check)) throw "REPR DOES NOT MATCH SHA256 HASH!!! LOOK OUT!!! TOUBLE AHEAD!!!";
+
         /* extra credit: encrypt the counter concatenated with the repr string, compute
         the checksum of the cipher text and compare it with the stored SHA-256 hash. */
         // text = bitarray_concat(string_to_bitarray(trusted_data_check), string_to_bitarray(repr));
         // hash_value = SHA256(enc_gcm(cipher, text));
-        // if (hash_value != priv.secrets.SHA_hash) throw "INVALID REPR!";
+        // if (!bitarray_equal(hash_value, repr["SHA_hash"])) throw "REPR DOES NOT MATCH SHA256 HASH!";
     }
 
-    priv.secrets.key_AUTH_message = bitarray_slice(lib.HMAC(master_key,"AUTH TO KEY"),0,128);
-    priv.secrets.key_HMAC_message = bitarray_slice(lib.HMAC(master_key,"HMAC TO KEY"),0,128);
-    priv.secrets.key_GCM_message = bitarray_slice(lib.HMAC(master_key,"GCM TO KEY"),0,128);
-    priv.secrets.salt = salt
-    /* parse the jason into keycahin. */    
-    priv.data = JSON.parse(repr)
-    delete priv.data['salt']
-    delete priv.data['auth_message']
-    // should reset keys here as well
-    ready = true;
+    priv.secrets.salt = salt;
+    priv.secrets.key_AUTH_message = key_AUTH_message;
+    priv.secrets.key_HMAC_message = bitarray_slice(lib.HMAC(master_key,"HMAC TO KEY"), 0, MAC_KEY_LENGTH_BITS);
+    priv.secrets.key_GCM_message = bitarray_slice(lib.HMAC(master_key,"GCM TO KEY"),   0, AES_KEY_LENGTH_BITS);
 
+    /* parse the json into keycahin. */    
+    priv.data = JSON.parse(repr);
+    /* the salt and auth_message should not be stored in the data (kvs), instead they're in
+    the secret object */
+    delete priv.data["salt"];
+    delete priv.data["auth_message"];
+    // delete priv.data["SHA_hash"];
+
+    ready = true;
     return true;
   };
 
@@ -154,20 +159,20 @@ var keychain = function() {
     * Return Type: array
     */ 
   keychain.dump = function() { /*N*/
-  	if(ready){
-		var data_json = JSON.stringify(priv.data)
-		//hacky way to make a deep copy of data map, since we don't
-		//want to add salt and auth_message to data field. 
-		var map_copy = JSON.parse(data_json)
-		
-		map_copy['salt'] = priv.secrets.salt
-		map_copy['auth_message'] = enc_gcm(setup_cipher(priv.secrets.key_AUTH_message),string_to_bitarray("AUTHENTICATE"))
-		var sha_check = lib.SHA256(string_to_bitarray(JSON.stringify(map_copy)))
-		var to_return = [JSON.stringify(map_copy),sha_check]
-		return to_return
-	}else{
-		return null
-	}
+  	if (ready) {
+  		/* Hacky way to make a deep copy of data map, since we don't want to add salt 
+      and auth_message to data field.  */
+  		var map_copy = JSON.parse(JSON.stringify(priv.data));
+  		map_copy["salt"] = priv.secrets.salt;
+  		map_copy["auth_message"] = enc_gcm(setup_cipher(priv.secrets.key_AUTH_message), string_to_bitarray("AUTHENTICATE"));
+
+  		var SHA_hash = lib.SHA256(string_to_bitarray(JSON.stringify(map_copy)));
+      // map_copy["SHA_hash"] = SHA_hash;
+
+      /* return the tuple */
+  		return [JSON.stringify(map_copy), SHA_hash];
+    }
+    return null;  
   }
 
   /**
@@ -181,25 +186,25 @@ var keychain = function() {
     * Return Type: string
     */
   keychain.get = function(name) { /*H*/
-    	if(ready){
-		var domain_key = priv.secrets.key_HMAC_message; 
-   		var value_key = priv.secrets.key_GCM_message;
-    		var domain_HMAC = lib.HMAC(domain_key,name); 
-		if(!(domain_HMAC in priv.data )){
-			return null;	
-		}
-		var encrypted = priv.data[domain_HMAC];
-		var plain_bits = dec_gcm(setup_cipher(value_key),encrypted);
-	 	var password_padded = bitarray_slice(plain_bits,0,(65+4)*8);
-		var password_text = string_from_padded_bitarray(password_padded,65);
-		if(! bitarray_equal(domain_HMAC,bitarray_slice(plain_bits,(65+4)*8,bitarray_len(plain_bits)))){
-			throw 'SWAPPING ATTACK DETECTED'
-		}
-		return password_text	
-	}else{
-		throw "Keychain not initialized."
+    if (ready) {
+		  var domain_key = priv.secrets.key_HMAC_message; 
+   	  var value_key = priv.secrets.key_GCM_message;
+    	var domain_HMAC = lib.HMAC(domain_key, name); 
+		  if(!(domain_HMAC in priv.data)) return null;	
+		
+      var encrypted = priv.data[domain_HMAC];
+      var plain_bits = dec_gcm(setup_cipher(value_key), encrypted);
+      var password_padded = bitarray_slice(plain_bits, 0, TOTAL_PASSWORD_LENGTH * 8);
+      var password_text = string_from_padded_bitarray(password_padded, MAX_PW_LEN_BYTES + 1);
+      if(! bitarray_equal(domain_HMAC, bitarray_slice(plain_bits, TOTAL_PASSWORD_LENGTH * 8, bitarray_len(plain_bits)))) {
+        throw "SWAPPING ATTACK DETECTED!!! LOOK OUT!!! TOUBLE AHEAD!!!"
+      }
+      return password_text;
+    } else {
+      throw "Keychain not initialized.";
+    }
 	}
-  }
+	
 
   /** 
   * Inserts the domain and associated data into the KVS. If the domain is
@@ -213,20 +218,19 @@ var keychain = function() {
   * Return Type: void
   */
   keychain.set = function(name, value) { /*N*/
-    if(ready){
-    	 var domain_key = priv.secrets.key_HMAC_message; 
-   	 var value_key = priv.secrets.key_GCM_message;
-    	 var domain_HMAC = lib.HMAC(domain_key,name);
-         var val_bits = string_to_padded_bitarray(value,65);
-	 var name_bits = domain_HMAC;
-	 //var signed_data = val_bits 
-	 var signed_data = bitarray_concat(val_bits,name_bits);
-	 var encrypted_data = enc_gcm(setup_cipher( value_key ) , signed_data);
-	 priv.data[domain_HMAC] = encrypted_data ;
-    }else{
-    	throw "Keychain not initialized."
+    if (ready) {
+      var domain_key = priv.secrets.key_HMAC_message; 
+      var value_key = priv.secrets.key_GCM_message;
+      var domain_HMAC = lib.HMAC(domain_key,name);
+      var val_bits = string_to_padded_bitarray(value, MAX_PW_LEN_BYTES + 1);
+    	var name_bits = domain_HMAC;
+    	
+      var signed_data = bitarray_concat(val_bits, name_bits);
+    	var encrypted_data = enc_gcm(setup_cipher(value_key), signed_data);
+    	priv.data[domain_HMAC] = encrypted_data;
+    } else {
+      throw "Keychain not initialized.";
     }
-    //throw "Not implemented!";
   }
 
   /**
@@ -239,17 +243,19 @@ var keychain = function() {
     * Return Type: boolean
   */
   keychain.remove = function(name) { /*H*/
-	if(ready){
-		var domain_key = priv.secrets.key_HMAC_message; 
-    		var domain_HMAC = lib.HMAC(domain_key,name);
-        	if(domain_HMAC in priv.data){
-			delete priv.data[domain_HMAC]
-			return true;
-		}
-		return false;
-	}else{
-		throw "Keychain not initialized."
-	}  
+  	if (ready) {
+  		var domain_key = priv.secrets.key_HMAC_message; 
+      var domain_HMAC = lib.HMAC(domain_key, name);
+      
+      if(domain_HMAC in priv.data) {
+  			delete priv.data[domain_HMAC];
+  			return true;
+  		}
+
+  		return false;
+  	} else {
+      throw "Keychain not initialized.";      
+    }
   }
 
   return keychain;
--- test-password-manager.js
+++ test-password-manager.js
@@ -1,5 +1,10 @@
 "use strict";
 
+var lib = require("./lib");
+
+var random_bitarray = lib.random_bitarray;
+
+
 function assert(condition, message) {
   if (!condition) {
     console.trace();
@@ -49,5 +54,100 @@ console.log("Checking contents of new database");
 for (var k in kvs) {
   assert(keychain.get(k) === new_keychain.get(k));
 }
+
+var setup = function(keychain){
+    keychain.init(password)
+    for (var k in kvs) {
+      keychain.set(k, kvs[k]);
+    }
+}
+
+// swap attack
+setup(keychain)
+var data = keychain.dump()[0]
+var map = JSON.parse(data)
+var keys = []
+for (var k in map) keys.push(k)
+var key0 = keys[0]
+var key1 = keys[1]
+var temp = map[key0]
+map[key0] = map[key1]
+map[key1] = temp
+data = JSON.stringify(map)
+keychain.load(password, data)
+
+var swap_detected = 0
+for (var service in kvs) {
+    try {
+        password = keychain.get(service)
+    }
+    catch(err) {
+        swap_detected ++
+    }
+}
+assert(swap_detected == 2)
+console.log("Swap attacks defended!!");
+
+// wrong password
+setup(keychain)
+var data = keychain.dump()[0]
+assert(!keychain.load('OBAMA', data))
+var error_count = 0
+try {
+    keychain.set("www.google.com", "123456")
+} catch(err) {
+    error_count ++
+}
+try {
+    keychain.get("www.google.com")
+} catch(err) {
+    error_count ++
+}
+try {
+    keychain.remove("www.google.com")
+} catch(err) {
+    error_count ++
+}
+assert(error_count == 3)
+assert(keychain.dump() == null)
+console.log("Data did not expose under wrong password.")
+
+// modifying data other than kvs
+setup(keychain)
+var data = keychain.dump()[0]
+var map = JSON.parse(data)
+map["salt"] = random_bitarray(128)
+data = JSON.stringify(map)
+assert(!keychain.load(password, data))
+
+setup(keychain)
+var data = keychain.dump()[0]
+var map = JSON.parse(data)
+map["salt"] = random_bitarray(64)
+data = JSON.stringify(map)
+assert(!keychain.load(password, data))
+
+setup(keychain)
+var data = keychain.dump()[0]
+var map = JSON.parse(data)
+map["auth_message"] = random_bitarray(128)
+data = JSON.stringify(map)
+assert(!keychain.load(password, data))
+console.log("Changing accessory data will disable the password manager!")
+
+// roll back attack
+keychain.EXTRA_CREDIT = false
+setup(keychain)
+var data_old = keychain.dump()
+keychain.set("service1", "apple")
+var data_new = keychain.dump()
+var invalid = false
+try {
+    keychain.load(password, data_old[0], data_new[1])
+} catch (err) {
+    invalid = true
+}
+assert(invalid)
+console.log("Roll back attact defended!!")
 
 console.log("All tests passed!");
